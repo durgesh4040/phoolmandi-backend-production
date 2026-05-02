e5250b8fe16ffa35eb6ea64a55bad35e2b516c07
durgesh <durgesh404011@gmail.com>
2026-05-02 23:53:44 +0530

    added transalation file

--- src/index.js
+++ src/index.js
@@ -6,12 +6,24 @@ import cors from 'cors';
 import user from './router/user.routes.js';
 import NodeCache from 'node-cache';
 import indexRoutes from "./router/index.routes.js"
+import i18n from 'i18n';
+import path from 'path';
+import { fileURLToPath } from 'url';
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 const app=express();
 app.use(express.json());
+i18n.configure({
+  locales: ['en', 'hi'],
+  directory: path.join(__dirname, 'translation'),
+  defaultLocale: 'en',
+  objectNotation: true,
+  queryParameter: 'lang'
+});
+app.use(i18n.init);
 app.use(cors());
 if (process.env.APP_ENV === "prod") {
   app.use("/",indexRoutes);
-
 } else {
    app.use("/api",indexRoutes);
 }
